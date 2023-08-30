38ad4d1fe8b1d345e2799291355ecd8283713524
boko-marc <marcboko.uriel@gmail.com>
2023-08-30 16:17:41 +0100

    set up architecture

---
+++ src/Books/Models/Books.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Core\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class Books extends Model
+{
+    use  HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+}
---
+++ src/Books/Repository/BooksRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Core\Books\Repository;
+
+use Core\Models\Books;
+use Core\Repository\Eloquent\BaseRepository;
+
+class BooksRepository extends BaseRepository implements BooksRepositoryInterface
+{
+
+    public function __construct(Books $books)
+    {
+        parent::__construct($books);
+    }
+}
